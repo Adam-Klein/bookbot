ca2ba7bbbfe91e01ee53ffc353fe5957e4e17041
alklein@gmail.com <alklein@gmail.com>
2025-04-29 11:14:48 -0700

    Update with additional books and sys.argv

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from stats import char_count, sorted_list
 import sys
 
 def main():
-    print(f"sys.argv is {sys.argv}")
-    book = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = get_book_text(sys.argv[1])
     num_words = count_words(book)
     print(f"{num_words} words found in the document")
     letter_counts = char_count(book)
